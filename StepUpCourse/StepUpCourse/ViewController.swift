88baed8d7002eca9238254cb1e6b2f9157171c44
olzu0217 <olga.zubrilina@netcracker.com>
2020-03-03 12:21:30 +0300

    first commit

---
+++ StepUpCourse/StepUpCourse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StepUpCourse
+//
+//  Created by Olga Zubrilina on 03.03.2020.
+//  Copyright © 2020 Olga Zubrilina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
