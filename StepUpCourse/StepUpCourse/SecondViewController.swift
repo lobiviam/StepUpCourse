c954257db0857f28e64d3cef51d25629ff74fd66
olzu0217 <olga.zubrilina@netcracker.com>
2020-03-03 13:49:51 +0300

    Remove UIScene support. Add navigation.

--- StepUpCourse/StepUpCourse/SecondViewController.swift
+++ StepUpCourse/StepUpCourse/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  StepUpCourse
 //
 //  Created by Olga Zubrilina on 03.03.2020.
@@ -8,15 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-    @IBAction func onGoPressed(_ sender: Any) {
-    }
-    
 }
-
